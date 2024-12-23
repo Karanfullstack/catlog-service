99104f8c67a0b9452e74960cd59bf3f65a0f73e6
Karan Chauhan <karanchouhan211@gmail.com>
2024-12-23 20:26:44 +0100

    add create product validator

--- src/product/product.validators.ts
+++ src/product/product.validators.ts
@@ -1,4 +1,5 @@
 import { body } from 'express-validator';
+import { ProductAttribute, ProductConfig } from './product.types';
 
 const createProductValidator = [
     body('name')
@@ -17,8 +18,75 @@ const createProductValidator = [
         .withMessage('Product image is required')
         .isString()
         .withMessage('Product image must be a string'),
-    body('priceConfiguration').exists().withMessage('Product priceConfiguration is required'),
-    body('attributes').exists().withMessage('Product attributes is required'),
+    body('priceConfiguration')
+        .isObject()
+        .withMessage('Product price configuration is required')
+        .custom((priceConfiguration: { [key: string]: ProductConfig }) => {
+            const keys = Object.keys(priceConfiguration);
+            if (!keys.length) {
+                throw new Error(
+                    'price configuration must have alteast one key example:"Size, Crust',
+                );
+            }
+            keys.forEach((key) => {
+                const config = priceConfiguration[key];
+                if (typeof config !== 'object') {
+                    throw new Error('price configuration value must be an object');
+                }
+                if (typeof config.priceType !== 'string') {
+                    throw new Error('priceType must be a string');
+                }
+                if (typeof config.avialableOptions !== 'object') {
+                    throw new Error('availableOptions must be an object');
+                }
+                Object.entries(config.avialableOptions).forEach(([optionsKey, optionValue]) => {
+                    if (typeof optionsKey !== 'string') {
+                        throw new Error(`${optionsKey as string}.avilableOptions key must string `);
+                    }
+
+                    if (typeof optionValue !== 'number') {
+                        throw new Error(
+                            `${optionValue as number}.avilableOptions key must number `,
+                        );
+                    }
+                });
+            });
+            return true;
+        }),
+    body('attributes')
+        .isArray({ min: 1 })
+        .withMessage('attributes must not be empty')
+        .custom((attributes: ProductAttribute[]) => {
+            return attributes.every((attribute) => {
+                if (typeof attribute !== 'object') {
+                    throw new Error('Each attribute must be an object');
+                }
+                if (typeof attribute.name !== 'string') {
+                    throw new Error('Each attribute must have a valid name of type string.');
+                }
+                if (!('value' in attribute)) {
+                    throw new Error('Each attribute must have a value field.');
+                }
+                return true;
+            });
+        }),
+    body('attributes.*.name')
+        .isString()
+        .withMessage('attribute name must be a string')
+        .notEmpty()
+        .withMessage('attribute name cannot be emppty'),
+    body('attributes.*.value')
+        .custom((value) => {
+            if (
+                typeof value !== 'boolean' &&
+                typeof value !== 'string' &&
+                typeof value !== 'number'
+            ) {
+                throw new Error('Attribute value must be a boolean, string, or number.');
+            }
+            return true;
+        })
+        .withMessage('Invalid value for attribute'),
 ];
 
 export { createProductValidator };
