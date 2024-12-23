cb0c665f16e39ef96d8a1619b1d3e21ac7cf2fec
Karan Chauhan <karanchouhan211@gmail.com>
2024-12-23 21:12:42 +0100

    add upload middleware

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,12 @@ import { globalErrorHandler } from './middlewares/globalError';
 import { categoryRouter } from './category/category.routes';
 import cookieParser from 'cookie-parser';
 import { productRouter } from './product/product.routes';
+import path from 'path';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use(express.static(path.join(__dirname, '../uploads')));
 app.use('/category', categoryRouter);
 app.use('/product', productRouter);
 app.use(globalErrorHandler);
