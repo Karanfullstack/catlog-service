f4d2a94f89dbd0ff27509c8508e4d3cac4281540
Karan Chauhan <karanchouhan211@gmail.com>
2024-12-25 22:48:27 +0100

    add fix create product validation

--- src/product/product.validators.ts
+++ src/product/product.validators.ts
@@ -18,58 +18,53 @@ const createProductValidator = [
         .withMessage('Product image is required')
         .isString()
         .withMessage('Product image must be a string'),
-    body('priceConfiguration')
-        .isObject()
-        .withMessage('Product price configuration is required')
-        .custom((priceConfiguration: { [key: string]: ProductConfig }) => {
-            const keys = Object.keys(priceConfiguration);
-            if (!keys.length) {
-                throw new Error(
-                    'price configuration must have alteast one key example:"Size, Crust',
-                );
+    body('priceConfiguration').custom((priceConfiguration: string) => {
+        const payload = JSON.parse(priceConfiguration) as { [key: string]: ProductConfig };
+        const keys = Object.keys(payload);
+        if (!keys.length) {
+            throw new Error('price configuration must have alteast one key example:"Size, Crust');
+        }
+        keys.forEach((key) => {
+            const config = payload[key];
+            if (typeof config !== 'object') {
+                throw new Error('price configuration value must be an object');
             }
-            keys.forEach((key) => {
-                const config = priceConfiguration[key];
-                if (typeof config !== 'object') {
-                    throw new Error('price configuration value must be an object');
+            if (typeof config.priceType !== 'string') {
+                throw new Error('priceType must be a string');
+            }
+            if (typeof config.avialableOptions !== 'object') {
+                throw new Error('availableOptions must be an object');
+            }
+            Object.entries(config.avialableOptions).forEach(([optionsKey, optionValue]) => {
+                if (typeof optionsKey !== 'string') {
+                    throw new Error(`${optionsKey as string}.avilableOptions key must string `);
                 }
-                if (typeof config.priceType !== 'string') {
-                    throw new Error('priceType must be a string');
-                }
-                if (typeof config.avialableOptions !== 'object') {
-                    throw new Error('availableOptions must be an object');
-                }
-                Object.entries(config.avialableOptions).forEach(([optionsKey, optionValue]) => {
-                    if (typeof optionsKey !== 'string') {
-                        throw new Error(`${optionsKey as string}.avilableOptions key must string `);
-                    }
 
-                    if (typeof optionValue !== 'number') {
-                        throw new Error(
-                            `${optionValue as number}.avilableOptions key must number `,
-                        );
-                    }
-                });
+                if (typeof optionValue !== 'number') {
+                    throw new Error(`${optionValue as number}.avilableOptions key must number `);
+                }
             });
+        });
+        return true;
+    }),
+    body('attributes').custom((attributes: string) => {
+        const payload = JSON.parse(attributes) as ProductAttribute[];
+        if (!Array.isArray(payload)) {
+            throw new Error('attributes must be an array');
+        }
+        return payload.every((attribute) => {
+            if (typeof attribute !== 'object') {
+                throw new Error('Each attribute must be an object');
+            }
+            if (typeof attribute.name !== 'string') {
+                throw new Error('Each attribute must have a valid name of type string.');
+            }
+            if (!('value' in attribute)) {
+                throw new Error('Each attribute must have a value field.');
+            }
             return true;
-        }),
-    body('attributes')
-        .isArray({ min: 1 })
-        .withMessage('attributes must not be empty')
-        .custom((attributes: ProductAttribute[]) => {
-            return attributes.every((attribute) => {
-                if (typeof attribute !== 'object') {
-                    throw new Error('Each attribute must be an object');
-                }
-                if (typeof attribute.name !== 'string') {
-                    throw new Error('Each attribute must have a valid name of type string.');
-                }
-                if (!('value' in attribute)) {
-                    throw new Error('Each attribute must have a value field.');
-                }
-                return true;
-            });
-        }),
+        });
+    }),
     body('attributes.*.name')
         .isString()
         .withMessage('attribute name must be a string')
