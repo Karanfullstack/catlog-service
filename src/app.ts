918c38aa226051e8a75a69420c5596246312c316
Karan Chauhan <karanchouhan211@gmail.com>
2024-12-30 22:28:32 +0100

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,19 @@ import { globalErrorHandler } from './middlewares/globalError';
 import { categoryRouter } from './category/category.routes';
 import cookieParser from 'cookie-parser';
 import { productRouter } from './product/product.routes';
+import cors from 'cors';
 import path from 'path';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../uploads')));
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    }),
+);
 app.use('/category', categoryRouter);
 app.use('/product', productRouter);
 app.use(globalErrorHandler);
